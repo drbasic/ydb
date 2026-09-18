d82eec2cc8934c3007fb35b3b77f6fdf676befbb
Kirill Pleshivtsev <drbasic@yandex-team.ru>
2026-09-18 15:57:38 +0700

    partition_direct: use behind as the only freshness map

--- ydb/core/nbs/cloud/blockstore/libs/storage/partition_direct/dirty_map/dirty_map.cpp
+++ ydb/core/nbs/cloud/blockstore/libs/storage/partition_direct/dirty_map/dirty_map.cpp
@@ -708,7 +708,7 @@ void TBlocksDirtyMap::FlushCompleted(
     const TInflightInfo& inflight,
     THostMask ddisks)
 {
-    AddToAheadAndBehindOnFlushCompleted(inflight, ddisks);
+    UpdateBehindOnFlushCompleted(inflight, ddisks);
 }
 
 void TBlocksDirtyMap::DataToPBufferAdded(
@@ -756,7 +756,7 @@ void TBlocksDirtyMap::DataFromPBufferReleased(
     }
 }
 
-void TBlocksDirtyMap::OnBehindAheadChanged()
+void TBlocksDirtyMap::OnBehindChanged()
 {
     ++StateGeneration;
 }
@@ -773,9 +773,7 @@ bool TBlocksDirtyMap::NeedErase() const
 
 bool TBlocksDirtyMap::NeedPersist() const
 {
-    const bool needPersistAheadBehind =
-        StateGeneration > PersistedStateGeneration;
-    return needPersistAheadBehind;
+    return StateGeneration > PersistedStateGeneration;
 }
 
 TDirtyMapStateProto TBlocksDirtyMap::GetStateForPersist() const
@@ -955,19 +953,6 @@ TString TBlocksDirtyMap::DebugPrintReadyToErase() const
     return result;
 }
 
-TString TBlocksDirtyMap::DebugPrintAhead() const
-{
-    TStringBuilder result;
-    for (THostIndex h = 0; h < GetHostCount(); ++h) {
-        auto ahead = DDiskStates[h].DebugPrintAhead();
-        if (ahead.empty()) {
-            continue;
-        }
-        result << "  " << PrintHostIndex(h) << ": " << ahead << "\n";
-    }
-    return result;
-}
-
 TString TBlocksDirtyMap::DebugPrintBehind() const
 {
     TStringBuilder result;
@@ -981,13 +966,13 @@ TString TBlocksDirtyMap::DebugPrintBehind() const
     return result;
 }
 
-TString TBlocksDirtyMap::DebugPrintAheadBehindBrief() const
+TString TBlocksDirtyMap::DebugPrintBehindBrief() const
 {
     TStringBuilder result;
     result << "gen:" << GetCurrentGeneration() << "/"
            << PersistedStateGeneration << " ";
     for (THostIndex h = 0; h < GetHostCount(); ++h) {
-        auto brief = DDiskStates[h].DebugPrintAheadBehindBrief();
+        auto brief = DDiskStates[h].DebugPrintBehindBrief();
         if (brief) {
             result << PrintHostIndex(h) << ":" << brief;
         }
@@ -1068,12 +1053,12 @@ TReadRangeHint TBlocksDirtyMap::MakeReadRangeHint(
                             : TRangeLock(weak_from_this(), pBufferKey));
 }
 
-void TBlocksDirtyMap::AddToAheadAndBehindOnFlushCompleted(
+void TBlocksDirtyMap::UpdateBehindOnFlushCompleted(
     const TInflightInfo& inflight,
     THostMask ddisks)
 {
-    // Check that one of the ddisks is lagging or aheading, in this case it
-    // needs to be notified about the data flush to ddisk.
+    // Check that one of the DDisks tracks missing data. In this case it needs
+    // to be notified about the data flush to DDisk.
     bool needNotify = AnyOf(
         DDiskStates,
         [](const TDDiskState& ddisk) { return ddisk.IsTrackingEnabled(); });
